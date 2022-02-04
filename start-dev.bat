07db2c6213b1d683ebf61af9c4316e24bf6b7c42
elfriedekocht <elfriedekocht@freenet.de>
2022-02-04 20:31:34 +0100

    Moved files to doc subfolder

--- start-dev.bat
+++
@@ -1,4 +0,0 @@
-start /min deno-file-server
-start /min cmd.exe /k "cd /d d:\github\elfriede-kocht"
-d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\elfriede-kocht\"
-start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/index.htm"
